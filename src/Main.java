d80e653961b290d610b77d60646237a8043335ac
maddie <mhoyle2011@hotmail.co.uk>
2023-02-07 18:16:03 +0000

    Added WindowManager
    modified:   src/Main.java
    new file:   src/WindowManager.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+class Main{
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
---
+++ src/WindowManager.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+
+class WindowManager{
+
+
+
+    public WindowManager(){
+
+    }
+}
