bbecc76e956ca2188b4d34f2ee0c3a936ecdf323
maddie <mhoyle2011@hotmail.co.uk>
2023-02-07 19:16:05 +0000

    Added Constructors For Main/WindowManager
    Also added information to run the WindowManager class in Main, has a automatic close timer too.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 class Main{
 
     public static void main(String[] args){
+        WindowManager manager;
 
+        System.out.println("Hello, world!");
+
+        manager = new WindowManager("Program!", 1920, 1080);
+
+        Timer t = new Timer();
+        TimerTask tsk = new TimerTask() {
+            @Override
+            public void run() {
+                boolean code;
+                code = manager.Quit();
+                System.out.println("Status Code: "+code);
+            }
+        };
+        t.schedule(tsk, 1000);
 
 
     }
--- src/WindowManager.java
+++ src/WindowManager.java
@@ -1,11 +1,29 @@
+import javax.swing.*;
 import java.awt.*;
-
 
 class WindowManager{
 
+    Frame root;
+    public WindowManager(String title, int width, int height){
 
+        Rectangle bounds;
 
-    public WindowManager(){
+        bounds = new Rectangle();
+        bounds.setSize(width, height);
 
+        root = new JFrame();
+        root.setTitle(title);
+
+        root.setBounds(bounds);
+
+        root.setVisible(true);
     }
+
+    public boolean Quit(){
+        // Returns True of exits safely, false if not.
+
+        root.dispose();
+        return true;
+    }
+
 }
